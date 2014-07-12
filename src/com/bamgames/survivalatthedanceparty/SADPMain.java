f0b05289fb424d12cffc0c0f7f3557eda54ee9a6
Beanxxbot <btdrape@gmail.com>
2014-07-12 13:43:01 -0500

    Version 0.0.0         Messing with Main Class                 --Timer Experiments                 --Basic Printing         To Do List                 --Added Checkboxes

--- src/com/bamgames/survivalatthedanceparty/SADPMain.java
+++ src/com/bamgames/survivalatthedanceparty/SADPMain.java
@@ -1,10 +1,33 @@
 package com.bamgames.survivalatthedanceparty;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+
 public class SADPMain
 {
+    static ActionListener listener = new ActionListener()
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+
+        }
+    };
+    static Timer timer = new Timer(1000, listener);
+
     public static void SADPMain()
     {
-        System.out.println("Rug");
+        timer.start();
+        if (timer.isRunning())
+        {
+            System.out.println("Running");
+        }
+        else
+        {
+            System.out.println("Not Running");
+        }
+        //System.out.println("Timer");
     }
     public static void main(String[] args)
     {
