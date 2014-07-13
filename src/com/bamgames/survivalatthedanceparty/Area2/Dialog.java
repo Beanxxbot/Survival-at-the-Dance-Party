d1fe5d5057aecb664f4751867145f1b92f1de1e6
Sirawesomeknight <hulsizerjames@gmail.com>
2014-07-12 23:48:58 -0500

    Version 0.0.1        Added Scanner to Organize When to Display Area        Renamed Dialog in Package area2 to Dialog2

--- src/com/bamgames/survivalatthedanceparty/Area2/Dialog.java
+++
@@ -1,6 +0,0 @@
-package com.bamgames.survivalatthedanceparty.Area2;
-import static com.bamgames.survivalatthedanceparty.speaking.Speaking.wizardSpeak;
-import static com.bamgames.survivalatthedanceparty.speaking.Speaking.playerSpeak;
-public class Dialog {
-
-}
---
+++ src/com/bamgames/survivalatthedanceparty/Area2/Dialog2.java
@@ -0,0 +1,8 @@
+package com.bamgames.survivalatthedanceparty.Area2;
+
+public class Dialog2 {
+    public static void hello()
+    {
+        System.out.println("hello");
+    }
+}
