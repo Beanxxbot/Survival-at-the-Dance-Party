7d86d432f313a4545991654241cee0f909cb6b4e
Beanxxbot <btdrape@gmail.com>
2014-10-08 09:54:31 -0500

    Bored at school - Doing nitpicky things

--- src/com/bamgames/survivalatthedanceparty/areathree/AreaThreeDialog.java
+++ src/com/bamgames/survivalatthedanceparty/areathree/AreaThreeDialog.java
@@ -1,4 +1,12 @@
 package com.bamgames.survivalatthedanceparty.areathree;
 
-public class AreaThreeDialog {
+import com.bamgames.survivalatthedanceparty.util.*;
+
+public class AreaThreeDialog 
+{
+  public static void highway()
+  {
+    debug("Guided walk to highway")
+    debug("")
+  }
 }
