42682311e9ce0091dc681d4f575c0a7925be808e
Beanxxbot <btdrape@gmail.com>
2014-10-08 09:14:06 -0500

    Bored at school - Doing nitpicky things

--- src/com/bamgames/survivalatthedanceparty/areatwo/AreaTwoDialog.java
+++ src/com/bamgames/survivalatthedanceparty/areatwo/AreaTwoDialog.java
@@ -9,25 +9,25 @@ public class AreaTwoDialog
     public static void dHouse()
     {
     //Make screen blur
-    debug("Its a blur..");
+    debug("Its a blur...");
     System.out.println("???: HELLO THERE!");
     playerSpeak("I had the strangest dream");
     System.out.println("???: What dream would that be?");
-    playerSpeak("I was in a strange version of hell. It was nothing what I thought it was.");
+    playerSpeak("I was in a strange version of Hell. It was nothing what I thought it was.");
     //Wizard is smiling
-    System.out.println("???: Was it different from this hell?");
-    playerSpeak("I'm still in hell.");
-    System.out.println("???: Oh that type of dream. It's alright the first-timers have those dreams the first sleep. It's typical.");
+    System.out.println("???: Was it different from this Hell?");
+    playerSpeak("I'm still in Hell.");
+    System.out.println("???: Oh that type of dream. It's alright, the newcomers usually have those dreams the first time they sleep. It's typical.");
     playerSpeak("Oh");
     System.out.println("???: I'm sorry I should've introduced myself. I'm Wiz D.");
-    playerSpeak("Nice to meet you I think.");
-    wizardSpeak("Yea this is hell though. There is nothing nice in hell.");
+    playerSpeak("Nice to meet you, I think.");
+    wizardSpeak("Yeah this is Hell though. There is nothing nice in hell.");
     playerSpeak("Anyways I have a bunch of questions. Door Guard didn't tell me much besides the basics.");
     wizardSpeak("Oh... Well what do you want to know?");
     AreaTwoDialogQuestions.initialQuestions();
     debug("Wiz D gives player basic items here");
     //Area2 Finished
-    debug("Area 2 Complete");
+    debug("Area 2 Complete!");
         SADPMain.nextArea();
     }
 
